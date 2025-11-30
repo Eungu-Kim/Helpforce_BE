fa95cb592bf53e4c70f5a43aa13abd20e8661970
eunsu Kim <eses816@gmail.com>
2025-11-30 22:42:14 +0900

    Initial commit: 프로젝트 초기 설정 및 로그인/회원가입 기능 구현

---
+++ src/main/java/com/web/helpforce/HelpforceApplication.java
@@ -0,0 +1,13 @@
+package com.web.helpforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpforceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpforceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/helpforce/HelpforceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.helpforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpforceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
